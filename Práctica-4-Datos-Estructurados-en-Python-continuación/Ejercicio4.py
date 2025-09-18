2c814a0eba63b49cc9b26041c2a37c0182140388
bryanflores764 <bryanaviles604@gmail.com>
2025-09-18 08:23:11 -0600

    Practicas

--- Práctica-4-Datos-Estructurados-en-Python-continuación/Ejercicio4.py
+++ Práctica-4-Datos-Estructurados-en-Python-continuación/Ejercicio4.py
@@ -1,20 +1,20 @@
-class Cola:
+class cola:
     def __init__(self):
         self.elementos = []
-
+    
     def esta_vacia(self):
         return len(self.elementos) == 0
-
+    
     def encolar(self, item):
         self.elementos.append(item)
-
+    
     def desencolar(self):
         return self.elementos.pop(0) if not self.esta_vacia() else "Cola vacía"
-    
-cola_estudiantes = Cola()
-cola_estudiantes.encolar("Sofia")
-cola_estudiantes.encolar("Mario")
-cola_estudiantes.encolar("lucia")    
 
-print("Entrega primero: ", cola_estudiantes.desencolar())
-print("Quedan en fila: ", cola_estudiantes.elementos)
+cola_red = cola()
+cola_red.encolar("Archivo.zip")
+cola_red.encolar("Archivo.mp3")
+cola_red.encolar("Archivo.pdf")
+
+print("Se transmite:", cola_red.desencolar())
+print("En cola de transmisión:", cola_red.elementos)
